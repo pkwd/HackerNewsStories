87fb48bf3feb0f0339f8fdeb2d3b55dd8a0ea5a2
pkwd <pkwd@PKWD-MAIN>
2025-02-10 11:15:49 +0100

    Initial commit

---
+++ Converters/HackerNewsStoryConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace HackerNewsStories.Converters
+{
+    using System;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using HackerNewsStories.Models;
+
+    public class HackerNewsStoryConverter : JsonConverter<HackerNewsStory>
+    {
+        public override HackerNewsStory Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            var element = document.RootElement;
+
+            var story = new HackerNewsStory();
+
+            if (element.TryGetProperty("title", out var title))
+            {
+                story.Title = title.GetString();
+            }
+            if (element.TryGetProperty("url", out var url))
+            {
+                story.Uri = url.GetString();
+            }
+            if (element.TryGetProperty("by", out var by))
+            {
+                story.PostedBy = by.GetString();
+            }
+            if (element.TryGetProperty("score", out var score))
+            {
+                story.Score = score.GetInt32();
+            }
+            if (element.TryGetProperty("descendants", out var descendants))
+            {
+                story.CommentCount = descendants.GetInt32();
+            }
+            if (element.TryGetProperty("time", out var time))
+            {
+                story.SetUnixTime(time.GetInt64());
+            }
+
+            return story;
+        }
+
+        public override void Write(Utf8JsonWriter writer, HackerNewsStory value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("title", value.Title);
+            writer.WriteString("url", value.Uri);
+            writer.WriteString("by", value.PostedBy);
+            writer.WriteNumber("score", value.Score);
+            writer.WriteNumber("descendants", value.CommentCount);
+            writer.WriteString("time", value.Time);
+            writer.WriteEndObject();
+        }
+    }
+}
+
---
+++ Models/HackerNewsStory.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+using HackerNewsStories.Converters;
+
+namespace HackerNewsStories.Models
+{
+    [JsonConverter(typeof(HackerNewsStoryConverter))]
+    public class HackerNewsStory
+    {
+        public string? Title { get; set; }
+        public string? Uri { get; set; }      
+        public string? PostedBy { get; set; }
+        public int Score { get; set; }
+        public int CommentCount { get; set; }
+
+        private long _unixTime;
+
+        public string Time => DateTimeOffset
+            .FromUnixTimeSeconds(_unixTime)
+            .ToUniversalTime()
+            .ToString("yyyy-MM-ddTHH:mm:ss+00:00");
+
+        internal void SetUnixTime(long unixTime)
+        {
+            _unixTime = unixTime;
+        }
+    }
+}
+
